39334b248ddcbbd4f744889893746d3e672bfb3a
tomiock <ockier1@gmail.com>
2023-12-08 23:11:58 +0100

    changes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <pybind11/pybind11.h>
-#include <python3.11/Python.h>
+#include <python3.10/Python.h>
 #include "main.h"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
+class Animal {
+public:
+    virtual ~Animal() { }
+    virtual std::string go(int n_times) = 0;
+};
 
+class Dog : public Animal {
+public:
+    std::string go(int n_times) override {
+        std::string result;
+        for (int i=0; i<n_times; ++i){
+            result += "woof! ";
+        }
+        return result;
+    }    
+};
+
+
+std::string call_go(Animal *animal){
+    return animal->go(3);
+}
 
 namespace py = pybind11;
 
@@ -41,7 +61,14 @@ PYBIND11_MODULE(python_example, m) {
           Some explanation
     )pbdoc");
 
-    
+    py::class_<Animal>(m, "Animal")
+        .def("data", &Animal::go);
+
+    py::class_<Dog, Animal>(m, "Dog")
+        .def(py::init<>());
+
+    m.def("call_go", &call_go);
+
     
     #ifdef VERSION_INFO
         m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
